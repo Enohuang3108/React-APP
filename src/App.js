3f3a8aa1b5dc61efc8b2abe588441e751d1b3c61
Enohuang3108 <Enohuang3108@gmail.com>
2021-12-14 17:43:01 +0800

    Day5

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-function App(){
-    return "React APP"
+import { BrowserRouter, Switch,Route } from 'react-router-dom';
+
+import Header from './Heaher';
+
+function App() {
+    return (
+    <BrowserRouter>
+        <Header />
+        <Switch>
+            <Route path="/" exact>Home</Route>
+            <Route path="/signin" exact>註冊登入</Route>
+            
+        </Switch>
+    </BrowserRouter>
+    
+    );
 }
 
 export default App;
