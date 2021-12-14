f5cd420b7ccdfbc3c0bb1ff332952247517dddfb
Enohuang3108 <Enohuang3108@gmail.com>
2021-12-15 02:11:57 +0800

    Day6

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import { BrowserRouter, Switch,Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Header from './Heaher';
-
+import Signin from './pages/signin';
 function App() {
     return (
-    <BrowserRouter>
-        <Header />
-        <Switch>
-            <Route path="/" exact>Home</Route>
-            <Route path="/signin" exact>註冊登入</Route>
-            
-        </Switch>
-    </BrowserRouter>
-    
+        <BrowserRouter>
+            <Header />
+            <Switch>
+                <Route path="/" exact>Home</Route>
+                <Route path="/signin" exact><Signin /></Route>
+
+            </Switch>
+        </BrowserRouter>
+
     );
 }
 
---
+++ src/utils/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/compat/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDS9r14pivTg2x-Tl0Gh3A4rhBXJ4SD0lY",
+  authDomain: "react-app-9a027.firebaseapp.com",
+  projectId: "react-app-9a027",
+  storageBucket: "react-app-9a027.appspot.com",
+  messagingSenderId: "935127957235",
+  appId: "1:935127957235:web:1c799246a54ca832b68186",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
