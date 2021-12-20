8bc8aa8257c19259b33cfac6cba472a4d40d3772
Enohuang3108 <Enohuang3108@gmail.com>
2021-12-20 11:51:12 +0800

    Day12

--- src/pages/Reports.js
+++ src/pages/Reports.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { Grid } from "semantic-ui-react"; /* 分割元件 */
+import {
+  Grid,
+  Item,
+  Image,
+  Icon,
+  Container,
+} from "semantic-ui-react"; /* 分割元件 */
 import Topics from "../components/Topics";
 import firebase from "../utils/firebase";
 function Reports() {
@@ -12,25 +18,55 @@ function Reports() {
       .get()
       .then((collectionSnapshot) => {
         const data = collectionSnapshot.docs.map((docSnapshot) => {
-          return docSnapshot.data();
+          const id = docSnapshot.id;
+          return {
+            ...docSnapshot.data(),
+            id,
+          }; /* 解構return docSnapshot.data(); 將資料跟id合併 */
         });
         setPosts(data);
       });
   }, []);
   return (
-    <Grid>
-      <Grid.Row>
-        <Grid.Column width={3}>
-          <Topics />
-        </Grid.Column>
-        <Grid.Column width={10}>
-          {posts.map((post) => {
-            return <p>{post.title}</p>;
-          })}
-        </Grid.Column>
-        <Grid.Column width={3}>訓練目標</Grid.Column>
-      </Grid.Row>
-    </Grid>
+    <Container>
+      <Grid>
+        <Grid.Row>
+          <Grid.Column width={3}>
+            <Topics />
+          </Grid.Column>
+          <Grid.Column width={10}>
+            <Item.Group>
+              {posts.map((post) => {
+                return (
+                  <Item key={post.id}>
+                    <Item.Image
+                      src={
+                        post.imageUrl ||
+                        "https://react.semantic-ui.com/images/wireframe/image.png"
+                      }
+                    />
+                    <Item.Content>
+                      <Item.Meta>
+                        {post.author.photoURL ? (
+                          <Image src={post.author.photoURL} />
+                        ) : (
+                          <Icon name="user circle" />
+                        )}
+                        {post.topic}．{post.author.displayName || "使用者"}
+                      </Item.Meta>
+                      <Item.Header>{post.title}</Item.Header>
+                      <Item.Description>{post.content}</Item.Description>
+                      <Item.Extra>留言 0．讚 0</Item.Extra>
+                    </Item.Content>
+                  </Item>
+                );
+              })}
+            </Item.Group>
+          </Grid.Column>
+          <Grid.Column width={3}>訓練目標</Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Container>
   );
 }
 export default Reports;
